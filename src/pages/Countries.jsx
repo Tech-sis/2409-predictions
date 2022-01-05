ac0e5e7a5997f733be63206282b649722944fe03
Tech-sis <abilawonadeola@gmail.com>
2022-01-05 13:35:20 +0100

    Recreated the country's page

--- src/pages/Countries.jsx
+++ src/pages/Countries.jsx
@@ -1,64 +1,45 @@
 import React, { useState, useEffect } from 'react'
 import styles from '../styles/countries.module.css'
-import { List, Input, Avatar, Skeleton, } from 'antd'
-import { Button, Tooltip } from 'antd';
+import { List, Input, Avatar, Skeleton, Divider } from 'antd'
 import { SearchOutlined } from '@ant-design/icons';
 import InfiniteScroll from 'react-infinite-scroll-component'
-import axios from 'axios';
 
 const Countries = () => {
   const { Search } = Input;
-  // const onSearch = value => console.log(value);
-  // const [searchTerm, setSearchTerm] = useState('')
-  const [loading, setLoading] = useState(false)
-  const [country, setCountry] = useState([])
   const [searchFilter, setSearchFilter] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [data, setData] = useState([])
+
+  const loadMoreData = () => {
+    if (loading) {
+      return
+    }
+      setLoading(true);
+    fetch('https://randomuser.me/api/?results=10&inc=name,gender,email,nat,picture&noinfo')
+      .then(res => res.json())
+      .then(body => {
+        setData([...data, ...body.results]);
+        setLoading(false);
+      })
+      .catch(() => {
+        setLoading(false);
+      });
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true)
-      const response = await axios.get('https://restcountries.eu/rest/v2/all')
-      setCountry(response.data)
-      setLoading(false)
-    }
-    fetchData()
-  }, [])
-
-  // const loadMoreData = () => {
-  //   if (loading) {
-  //     return
-  //   }
-  //   setLoading(true)
-  //   fetch(
-  //     'https://randomuser.me/api/?results=10&inc=name,gender,email,nat,picture&noinfo'
-  //   )
-  //     .then((res) => res.json())
-  //     .then((body) => {
-  //       setData([...data, ...body.results])
-  //       setLoading(false)
-  //     })
-  //     .catch(() => {
-  //       setLoading(false)
-  //     })
-  // }
-
-  // useEffect(() => {
-  //   loadMoreData()
-  // }, [])
-
-  // const handleChange = (e) => {
-  //   setSearchTerm(e.target.value)
-  // }
+    loadMoreData();
+  }, []);
 
   return (
     <div className={styles.container}>
       <div id="scrollableDiv" className={styles.scrollableDiv}>
         {/* <div>Top Countries</div> */}
         <InfiniteScroll
-          dataLength={country.length}
-          // next={fetchMoreData}
-          hasMore={country.length < 50}
+          dataLength={data.length}
+          next={loadMoreData}
+          hasMore={data.length < 50}
           loader={<Skeleton avatar paragraph={{ rows: 1 }} active />}
+          endMessage={<Divider plain>It is all, nothing more 🤐</Divider>}
           scrollableTarget="scrollableDiv"
         >
           <List
@@ -67,38 +48,16 @@ const Countries = () => {
                <Search prefix={<SearchOutlined />} placeholder="Country" enterButton="" onChange={(e) => setSearchFilter(e.target.value)} />
             </div>
           }
-            dataSource={country}
+            dataSource={data}
             loadMore
-            // renderItem={(item) => (
-            //   <List.Item key={item.id}>
-            //     <List.Item.Meta
-            //       avatar={<Avatar src={item.picture.large} shape="square" size="large" />}
-            //       title={<a href="/country">{item.name.last}</a>}
-            //     />
-            //   </List.Item>
-            // )}
-            renderItem={loading ? (<h4>Loading</h4>
-              ) : (
-                country
-                .filter((value) => {
-              if (searchFilter === '') {
-                return value
-                } else if (value.title.toLowerCase().includes(searchFilter.toLowerCase())
-                ) {
-                  return value
-                }
-              })
-          .map((item) => {
-            return (
+            renderItem={item => (
             <List.Item key={item.id}>
               <List.Item.Meta
-                avatar={<Avatar src={item.flags} shape="square" size="large" />}
-                title={<a href="/country">{item.name.official}</a>}/>
+                avatar={<Avatar src={item.picture.large} />}
+                title={<a href="/country">{item.name.last}</a>}
+              />
             </List.Item>
-            )
-            
-          })
-            )}
+          )}
           />
         </InfiniteScroll>
       </div>
