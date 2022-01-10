0f8b57f8c0d245cf3c6fbef5d53d7caf72d62433
Tech-sis <abilawonadeola@gmail.com>
2022-01-10 11:17:21 +0100

    changed the pages file path in app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 // import './App.css';
 import React from 'react'
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 // Components
 // import NavHeader from './components/header'
 
 // Pages
-import Home from './pages/Home'
-import RegistrationPage from './pages/RegistrationPage'
-import Fixture from './pages/Fixturehome'
-import Sports from './pages/Sports'
-import Countries from './pages/Countries'
-import MajorCountry from './pages/MajorCountry'
+import Home from '../src/pages/Home'
+import RegistrationPage from '../src/pages/RegistrationPage'
+import Fixture from '../src/pages/Fixturehome'
+import Sports from '../src/pages/Sports'
+import Countries from '../src/pages/Countries'
+import MajorCountry from '../src/pages/MajorCountry'
 
 function App() {
-
   return (
     <div>
       <Router>
