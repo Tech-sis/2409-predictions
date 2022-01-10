d5335f034be1fdbd6222654c386f07af7891df61
Tech-sis <abilawonadeola@gmail.com>
2022-01-10 11:23:48 +0100

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import './App.css';
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
@@ -6,12 +5,12 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 // import NavHeader from './components/header'
 
 // Pages
-import Home from '../src/pages/Home'
-import RegistrationPage from '../src/pages/RegistrationPage'
-import Fixture from '../src/pages/Fixturehome'
-import Sports from '../src/pages/Sports'
-import Countries from '../src/pages/Countries'
-import MajorCountry from '../src/pages/MajorCountry'
+import Home from './pages/Home'
+import RegistrationPage from './pages/RegistrationPage'
+import Fixture from './pages/Fixturehome'
+import Sports from './pages/Sports'
+import Countries from './pages/Countries'
+import MajorCountry from './pages/MajorCountry'
 
 function App() {
   return (
